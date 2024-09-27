ed13f5b0fcb26280837786991aa3c1903e01a007
Dustin Peukert <dpeukert04@gmail.com>
2024-09-27 17:00:14 -0400

    write 2 tests following the prompts in pirate_spec.rb

--- ruby_exercises/mythical-creatures/spec/pirate_spec.rb
+++ ruby_exercises/mythical-creatures/spec/pirate_spec.rb
@@ -40,12 +40,21 @@ RSpec.describe Pirate do
   it 'has a booty' do
     # create a pirate
     # check that the pirate starts with 0 booty
+    pirate = Pirate.new('Bluebeard')
+    expect(pirate.booty).to eq (0)
   end
 
   it 'gets 100 booty for robbing a ship' do
     # create a pirate
     # rob some ships
     # check that the pirate got 100 booty for each ship it robbed
+    pirate = Pirate.new('Redbeard')
+    
+    3.times do
+      pirate.rob_a_ship
+    end
+
+    expect(pirate.booty).to eq(300)
   end
 
 end
