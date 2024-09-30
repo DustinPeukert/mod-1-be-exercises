b4263ebc16d5fe5e83875cbd32a5795db28308bd
Dustin Peukert <dpeukert04@gmail.com>
2024-09-30 14:14:17 -0400

    remove Person class to add to person.rb

--- ruby_exercises/mythical-creatures/lib/medusa.rb
+++ ruby_exercises/mythical-creatures/lib/medusa.rb
@@ -17,17 +17,3 @@ class Medusa
     end
 
 end
-
-class Person
-    attr_reader :name
-    attr_accessor :is_stoned
-
-    def initialize(name)
-        @name = name
-        @is_stoned = false
-    end
-
-    def stoned?
-        @is_stoned
-    end
-end
